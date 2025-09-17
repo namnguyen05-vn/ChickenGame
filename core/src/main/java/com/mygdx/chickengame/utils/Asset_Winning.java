68ef839aef82a850d3579f130d4ae15e9bf7f121
Nguyen Hoang Nam <namnguyen05@example.com>
2025-09-17 23:00:16 +0700

    assets_update

--- core/src/main/java/com/mygdx/chickengame/utils/Asset_Winning.java
+++ core/src/main/java/com/mygdx/chickengame/utils/Asset_Winning.java
@@ -5,9 +5,16 @@ import com.badlogic.gdx.Gdx;
 public class Asset_Winning {
     public static Texture PlayAgain;
     public static Music BGMusic;
+    public static Texture backgroundTex;
 
     public static void load(){
+        backgroundTex = new Texture("Image/background_menu.png");
         PlayAgain = new Texture("Image/PlayAgainButton.png");
         BGMusic =  Gdx.audio.newMusic(Gdx.files.internal("Music/WinningSound.ogg"));
+    }
+    public static void dispose(){
+        backgroundTex.dispose();
+        PlayAgain.dispose();
+        BGMusic.dispose();
     }
 }
