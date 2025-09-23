71d849ee699845aef06446a9464f5e7c5e38d575
Nguyen Hoang Nam <namnguyen05@example.com>
2025-09-23 15:50:10 +0700

    update entities,screen 23/09/2025

---
+++ core/src/main/java/com/mygdx/chickengame/entities/Boss_Bullet.java
@@ -0,0 +1,38 @@
+package com.mygdx.chickengame.entities;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.mygdx.chickengame.utils.Assets_LV3;
+
+public class Boss_Bullet {
+    private Vector2 position;
+    private Vector2 velocity;
+    public Rectangle rect;
+    private float speed = 350; // tốc độ đạn px/s
+
+    //  nhận vị trí Boss và Player
+    public Boss_Bullet(float startX, float startY, float targetX, float targetY) {
+        position = new Vector2(startX, startY);
+        // Tính hướng từ Boss -> Player rồi nhân với speed
+        velocity = new Vector2(targetX - startX, targetY - startY).nor().scl(speed);
+
+        rect = new Rectangle(startX, startY, 15, 25);
+    }
+
+    public void update(float delta) {
+        // Cập nhật vị trí
+        position.add(velocity.x * delta, velocity.y * delta);
+
+        // Cập nhật lại
+        rect.setPosition(position.x, position.y);
+    }
+
+    public void render(SpriteBatch batch) {
+        batch.draw(Assets_LV3.BossBullet, rect.x, rect.y, rect.width, rect.height);
+    }
+
+    public Rectangle getRect() {
+        return rect;
+    }
+}
---
+++ core/src/main/java/com/mygdx/chickengame/screens/LevelBoss.java
@@ -0,0 +1,86 @@
+
+package com.mygdx.chickengame.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
+import com.mygdx.chickengame.ChickenGame;
+import com.mygdx.chickengame.entities.Boss_100;
+import com.mygdx.chickengame.entities.Boss_50;
+import com.mygdx.chickengame.entities.Boss_Bullet;
+import com.mygdx.chickengame.entities.Bullet;
+import com.mygdx.chickengame.entities.Enemy1;
+import com.mygdx.chickengame.entities.Enemy3;
+import com.mygdx.chickengame.entities.Player;
+import com.mygdx.chickengame.utils.Assets_Common;
+import com.mygdx.chickengame.utils.Assets_LV3;
+
+
+public class LevelBoss implements Screen {
+    private ChickenGame game;
+    private SpriteBatch batch;
+    private Player player;
+    private Array<Enemy1> enemies1;
+    private Array<Enemy3> enemies3;
+    private Array<Bullet> bullets;
+    private Array<Boss_Bullet> b_bullets;
+    private Boss_100 boss_100;
+    private Boss_50 boss_50;
+
+    public LevelBoss(ChickenGame game) {
+        this.game = game;
+        this.batch = new SpriteBatch();
+        this.player = new Player();
+        this.enemies1 = new Array<>();
+        this.enemies3 = new Array<>();
+        this.bullets = new Array<>();
+        this.b_bullets = new Array<>();
+
+        Assets_Common.load();
+        Assets_LV3.load();
+        Assets_LV3.BGMusic.play();
+
+        this.boss_100 = new Boss_100(player);
+
+    }
+
+    @Override
+    public void render(float delta) {
+        try {
+            Gdx.gl.glClearColor(0,0,0,1);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+            // update
+            player.update(delta, bullets);
+            for (Enemy1 e : enemies1) e.update(delta);
+            for (Bullet b : bullets) b.update(delta);
+            if(boss_100!=null)boss_100.update(delta);
+
+            // vẽ
+            batch.begin();
+            batch.draw(Assets_LV3.backgroundTex, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+            player.render(batch);
+            if(boss_100!=null) boss_100.render(batch);
+            for (Enemy1 e : enemies1) e.render(batch);
+            for (Bullet b : bullets) b.render(batch);
+
+            batch.end();
+        } catch (Exception e) {
+            Gdx.app.error("LevelBoss", "Error in render", e);
+        }
+    }
+
+
+    @Override public void show() {}
+    @Override public void resize(int width, int height) {}
+    @Override public void pause() {}
+    @Override public void resume() {}
+    @Override public void hide() {}
+    @Override public void dispose() {
+        batch.dispose();
+        Assets_LV3.dispose();
+    }
+
+}
